01f57d51b8af8fe70fea2c9b7aa52d21bbbff4f7
Kender ROMAIN <kenderromain@gmail.com>
2023-02-14 11:38:10 -0400

    Fix linter errors.

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -12,35 +12,55 @@ const Calculator = () => {
   };
   return (
     <div className="calculator-wrapper">
-      <div className="display">{result.total || result.next || 0}</div>
+      <div className="display">{result.next || result.total || 0}</div>
       <div className="row">
         <Button symbole="AC" handleClick={handleClick} />
         <Button symbole="+/-" handleClick={handleClick} />
         <Button symbole="%" handleClick={handleClick} />
-        <Button symbole="÷" color={operatorsBtnColor} handleClick={handleClick} />
+        <Button
+          symbole="÷"
+          color={operatorsBtnColor}
+          handleClick={handleClick}
+        />
       </div>
       <div className="row">
         <Button symbole="7" handleClick={handleClick} />
         <Button symbole="8" handleClick={handleClick} />
         <Button symbole="9" handleClick={handleClick} />
-        <Button symbole="x" color={operatorsBtnColor} handleClick={handleClick} />
+        <Button
+          symbole="x"
+          color={operatorsBtnColor}
+          handleClick={handleClick}
+        />
       </div>
       <div className="row">
         <Button symbole="4" handleClick={handleClick} />
         <Button symbole="5" handleClick={handleClick} />
         <Button symbole="6" handleClick={handleClick} />
-        <Button symbole="-" color={operatorsBtnColor} handleClick={handleClick} />
+        <Button
+          symbole="-"
+          color={operatorsBtnColor}
+          handleClick={handleClick}
+        />
       </div>
       <div className="row">
         <Button symbole="1" handleClick={handleClick} />
         <Button symbole="2" handleClick={handleClick} />
         <Button symbole="3" handleClick={handleClick} />
-        <Button symbole="+" color={operatorsBtnColor} />
+        <Button
+          symbole="+"
+          color={operatorsBtnColor}
+          handleClick={handleClick}
+        />
       </div>
       <div className="row">
         <Button symbole="0" flex="2.55" handleClick={handleClick} />
         <Button symbole="." handleClick={handleClick} />
-        <Button symbole="=" color={operatorsBtnColor} handleClick={handleClick} />
+        <Button
+          symbole="="
+          color={operatorsBtnColor}
+          handleClick={handleClick}
+        />
       </div>
     </div>
   );
