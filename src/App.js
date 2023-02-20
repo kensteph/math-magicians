1980f3e83533563c1e4828ce88926f9e9cfaa58f
Kender ROMAIN <kenderromain@gmail.com>
2023-02-20 18:47:30 -0400

    Create the Quote page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import CalculatorPage from './pages/CalculatorPage/CalculatorPage';
 import Home from './pages/Home/Home';
+import Quote from './pages/Quote/Quote';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<Quote />} />
       </Routes>
     </div>
   );
