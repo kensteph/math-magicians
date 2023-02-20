b830e2eb10f0461eacf75b2103495ed2e6eab634
Kender ROMAIN <kenderromain@gmail.com>
2023-02-20 19:34:01 -0400

    Add background image.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,42 +1,12 @@
 import React from 'react';
-import './home.module.css';
+import styles from './home.module.css';
 
 const Home = () => (
-  <section>
-    <h2>Welcome to our page!</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere
-      lacus vel malesuada euismod. Ut a tempus metus, vitae interdum quam.
-      Maecenas a aliquam lectus. Curabitur scelerisque ut ex quis dignissim.
-      Nunc pretium blandit malesuada. Ut a luctus ipsum. Duis semper semper
-      libero a malesuada. Nunc sit amet lorem id nisi sollicitudin sagittis.
-      Duis blandit nisi fermentum, tincidunt urna quis, cursus quam. Cras
-      iaculis varius dictum. Class aptent taciti sociosqu ad litora torquent per
-      conubia nostra, per inceptos himenaeos.Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit. Fusce posuere lacus vel malesuada euismod. Ut
-      a tempus metus, vitae interdum quam. Maecenas a aliquam lectus. Curabitur
-      scelerisque ut ex quis dignissim. Nunc pretium blandit malesuada. Ut a
-      luctus ipsum. Duis semper semper libero a malesuada. Nunc sit amet lorem
-      id nisi sollicitudin sagittis. Duis blandit nisi fermentum, tincidunt urna
-      quis, cursus quam. Cras iaculis varius dictum. Class aptent taciti
-      sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-    </p>
-    <p>
-      Quisque sagittis ex lobortis dui porta bibendum. Sed ultrices et nisi vel
-      feugiat. Nam efficitur pretium ullamcorper. Duis ultrices tempus tempus.
-      Nam eu sem odio. Sed eget libero non massa gravida hendrerit. Maecenas eu
-      sem lectus. Nullam fringilla accumsan iaculis. Donec nec arcu et mi
-      laoreet finibus eget quis diam. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Cras ullamcorper
-      sapien id ullamcorper semper. In volutpat nibh eget ex sagittis
-      tristique.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-      posuere lacus vel malesuada euismod. Ut a tempus metus, vitae interdum
-      quam. Maecenas a aliquam lectus. Curabitur scelerisque ut ex quis
-      dignissim. Nunc pretium blandit malesuada. Ut a luctus ipsum. Duis semper
-      semper libero a malesuada. Nunc sit amet lorem id nisi sollicitudin
-      sagittis. Duis blandit nisi fermentum, tincidunt urna quis, cursus quam.
-      Cras iaculis varius dictum. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos.
+  <section className={styles.section}>
+    <h2 className={styles.h2}>Welcome to our page!</h2>
+    <p className={styles.p}>
+      Math Magicians is a project that allows the users to make simple
+      calculations and read a ramdom math-related quote.
     </p>
   </section>
 );
